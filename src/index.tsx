62b84274f56814e0504bce020017aea7ec7e5a58
Araozu <faraoz@unsa.edu.pe>
2020-08-04 11:00:31 -0500

    Commit inicial.

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './sass/global.sass';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Inicio from './views/Index/Index';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Inicio />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/views/Index/Index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Index() {
+  return (
+    <div>
+      Pagina de inicio de HXtreme
+    </div>
+  );
+}
+
+export default Index;
